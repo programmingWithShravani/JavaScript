f0f1ccabbbb9fdbc84543ae9deda6b8e5c7bbac6
programmingWithShravani <shravanikanse012@gmail.com>
2026-07-16 15:07:57 +0530

    javascript addition and example

---
+++ Addition.js
@@ -0,0 +1,6 @@
+const prompt=require("prompt-sync")();
+const num1=parseFloat(prompt("Enter the first Number:"));
+const num2=parseFloat(prompt("Enter the second Number:"));
+
+const sum=num1+num2;
+console.log("The sum of num1 and num2 is="+sum);
