11be87f36a3321eb052caaccf8a6664f7c101f4c
programmingWithShravani <shravanikanse012@gmail.com>
2026-07-22 15:41:26 +0530

    Add examples of array methods including push and pop

---
+++ Array_Methods/arraymethod.js
@@ -0,0 +1,9 @@
+let fruits = ["Apple","mango","chikku","banana"];
+console.log(fruits.length);
+
+//push():--add elements into array.
+fruits.push("orange");
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
