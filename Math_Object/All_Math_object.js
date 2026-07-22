07f311e3e88ef9331e8b30f38e724e57df525466
programmingWithShravani <shravanikanse012@gmail.com>
2026-07-22 14:55:40 +0530

    Add examples of Math object methods including PI, rounding, absolute value, min/max, power, square root, and random number generation

---
+++ Math_Object/All_Math_object.js
@@ -0,0 +1,43 @@
+// 1.value of PI
+console.log(Math.PI);
+
+// 2.Round Nearest integer
+console.log(Math.round(7.3));
+
+// 3.Round Downwards
+console.log(Math.floor(4.9));
+
+// 4.Round Upwards
+console.log(Math.ceil(5.1));
+
+// 5.remove deicnmal(trunc())
+console.log(Math.trunc(4.9));
+
+// 6.absolute value:abs()
+console.log(Math.abs(-30));
+
+// 7.to find largest number
+console.log(Math.max(10,45,22,19));
+
+//8. to find smallest number
+console.log(Math.min(12,64,5,3,45,));
+
+//9.power 
+console.log(Math.pow(2,4));
+
+//10. exponent operator
+console.log(2**6);
+
+//11. to find square root 
+console.log(Math.sqrt(144));
+
+//12.to find cube root
+console.log(Math.cbrt(64));
+
+//13.random number between 0 and 1.
+console.log(Math.random());
+
+//14. rander integer between 1 to 10
+let random = Math.floor(Math.random()*10)+1;
+
+console.log(random);
