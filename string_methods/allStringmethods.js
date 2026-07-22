a52fe2d3e226e94dafe65a4a7d3f27836816580a
programmingWithShravani <shravanikanse012@gmail.com>
2026-07-22 14:36:19 +0530

    Add examples of string methods including length, toUpperCase, toLowerCase, trim, charAt, at, indexOf, lastIndexOf, startsWith, endsWith, slice, substring, replace, replaceAll, split, concat, and repeat

---
+++ string_methods/allStringmethods.js
@@ -0,0 +1,78 @@
+//length
+let str = "Hello Javascript";
+console.log(str.length);
+
+
+//to Uppercase
+let str1 = "Welcome to JSPM";
+console.log(str1.toUpperCase());
+
+//to Lowercase
+
+let str2 = "My Name Is";
+console.log(str2.toLowerCase());
+
+//4.trim
+let str3 = "  apple  ";
+console.log(str3.trim());
+
+
+//5.charAt
+let str4 = "hello";
+console.log(str4.charAt(3));
+
+
+//6.at()
+let str5 = "welcome";
+console.log(str5.at(-2));
+
+
+//7.indexOf()
+let str6 = "Javascript";
+console.log(str6.indexOf("s"));
+
+//8.lastindex()
+let str7 = "hello";
+console.log(str7.lastIndexOf("o"));
+
+
+//9.startWith()
+let str8 = "javascript";
+console.log(str8.startsWith("java"));
+
+
+//10.endswith()
+let str9 = "javascript";
+console.log(str9.endsWith("script"));
+
+
+//11.slice()
+let str10 = "javascript";
+console.log(str10.slice(6,10));
+
+//12.substring
+let str11 = "programming";
+console.log(str11.substring(0,7));
+
+
+//13.replace
+let str12 = "Hello World";
+console.log(str12.replace("World","JS"));
+
+//14.replace all()
+let str13 = "yes yes yes";
+console.log(str13.replaceAll("yes","No"));
+
+//15.split()
+let str14 = "red,green,blue";
+console.log(str14.split(","));
+
+
+//16.concat()
+let a= "hello ";
+let b= "world";
+console.log(a.concat("",b));
+
+//17.repeat()
+let str15 = "Hello ";
+console.log(str15.repeat(5));
