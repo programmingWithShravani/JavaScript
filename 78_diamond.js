d80e16be9751218c1ed45642228713845f4ea49b
programmingWithShravani <shravanikanse012@gmail.com>
2026-07-23 22:27:22 +0530

    Add examples of various triangle and diamond patterns using loops

---
+++ 78_diamond.js
@@ -0,0 +1,21 @@
+let n = 5;
+
+// Upper Part
+for (let i = 1; i <= n; i++) {
+    let row = "";
+
+    for (let j = 1; j <= n - i; j++) row += " ";
+    for (let k = 1; k <= (2 * i - 1); k++) row += "*";
+
+    console.log(row);
+}
+
+// Lower Part
+for (let i = n - 1; i >= 1; i--) {
+    let row = "";
+
+    for (let j = 1; j <= n - i; j++) row += " ";
+    for (let k = 1; k <= (2 * i - 1); k++) row += "*";
+
+    console.log(row);
+}
